7db8a4a1cd276550324d49cd38dd08f2ad768329
PvelV <pvel.vaverka@centrum.cz>
2018-08-02 15:33:41 +0200

    Initial commit

---
+++ Fasetto/Common/Command.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Fasetto.Common
+{
+    public class Command<T> : ICommand
+    {
+        private Action<T> action;
+        private Predicate<T> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null ? true : canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action((T)parameter);
+        }
+
+        public Command(Action<T> _action, Predicate<T>_canExecute=null)
+        {
+            action = _action;
+            canExecute = _canExecute;
+        }
+    }
+}
---
+++ Fasetto/Common/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PropertyChanged;
+
+namespace Fasetto.Common
+{
+    [ImplementPropertyChanged]
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
