a868996d9c5b47aef75212586de3225475fe03e7
PvelV <pvel.vaverka@centrum.cz>
2018-08-02 16:49:29 +0200

    ViewModel fix

--- Fasetto/Common/ViewModel.cs
+++ Fasetto/Common/ViewModel.cs
@@ -5,18 +5,24 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using PropertyChanged;
 
 namespace Fasetto.Common
 {
-    [ImplementPropertyChanged]
     public class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
 
         public void OnPropertyChanged(string name)
         {
-            PropertyChanged(this, new PropertyChangedEventArgs(name));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public void SetProperty<T>(ref T backingField, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(backingField, value))
+            {
+                OnPropertyChanged(propertyName);
+            }
         }
     }
 }
