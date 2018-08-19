6418e27b17eb6244f9451ed69a81db6a11116eba
PvelV <pvel.vaverka@centrum.cz>
2018-08-19 18:26:32 +0200

    animations 2

--- Fasetto.Word.Core/ViewModels/Application/RegisterPageViewModel.cs
+++ Fasetto.Word.Core/ViewModels/Application/RegisterPageViewModel.cs
@@ -11,13 +11,38 @@ namespace Fasetto.Word.Core
     public class RegisterPageViewModel : ViewModel
     {
 
-  
+        public string Email { get; set; }
+
+        public bool LoginIsRunning { get; set; }
+
+        public Command LoginCommand { get; }
+        public Command RegisterCommand { get; }
+
 
 
         public RegisterPageViewModel()
         {
+            LoginCommand = new Command(async (param) => await LoginAsync(param));
+            RegisterCommand = new Command(async () => await RegisterAsync());
         }
 
-     
+
+
+        public async Task LoginAsync(object param)
+        {
+            await RunCommandAsync(() => LoginIsRunning, async () =>
+            {
+                await Task.Delay(5000);
+                var pass = (param as IHavePassword).SecurePassword.Unsecure();
+            });
+        }
+
+        public async Task RegisterAsync()
+        {
+            Container.Get<ApplicationViewModel>().IsSideMenuVisible ^= true;
+
+            Container.Get<ApplicationViewModel>().CurrentPage = ApplicationPage.RegisterPage;
+            await Task.Delay(1);
+        }
     }
 }
--- Fasetto/Pages/BasePage.cs
+++ Fasetto/Pages/BasePage.cs
@@ -10,17 +10,72 @@ using System.Windows.Media.Animation;
 
 namespace Fasetto.Word
 {
-    public class BasePage<VM> : Page where VM : ViewModel, new()
+    public class BasePage : Page
     {
-        private VM mViewModel;
-
-        public bool ShouldAnimateOut { get; set; }
 
         public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
         public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+        public float SlideSeconds { get; set; } = 0.4f;
+        public bool ShouldAnimateOut { get; set; }
 
-        public float SlideSeconds { get; set; } = 0.8f;
+        public BasePage()
+        {
+            if (PageLoadAnimation != PageAnimation.None)
+            {
+                Visibility = Visibility.Collapsed;
+            }
+            Loaded += BasePage_LoadedAsync;
 
+        }
+
+        private async void BasePage_LoadedAsync(object sender, RoutedEventArgs e)
+        {
+            if (ShouldAnimateOut)
+                // Animate out the page
+                await AnimateOutAsync();
+            // Otherwise...
+            else
+                // Animate the page in
+                await AnimateInAsync();
+        }
+
+        public async Task AnimateInAsync()
+        {
+            if (PageLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+
+                    await this.SlideAndFadeInFromRightAsync(SlideSeconds);
+
+                    break;
+                                              
+            }
+        }
+
+        public async Task AnimateOutAsync()
+        {
+            if (PageUnloadAnimation == PageAnimation.None)
+                return;
+
+            switch (PageUnloadAnimation)
+            {
+            
+                case PageAnimation.SlideAndFadeOutToLeft:
+
+                    await this.SlideAndFadeOutToLeftAsync(SlideSeconds);
+
+                    break;
+            }
+        }
+    }
+
+    public class BasePage<VM> : BasePage where VM : ViewModel, new()
+    {
+        private VM mViewModel;
+            
         public VM ViewModel
         {
             get { return mViewModel; }
@@ -35,68 +90,13 @@ namespace Fasetto.Word
             }
         }
 
-        public BasePage()
+        public BasePage() : base()
         {
-            if (PageLoadAnimation != PageAnimation.None)
-            {
-                Visibility = Visibility.Collapsed;
-            }
-            Loaded += BasePage_LoadedAsync;
-
+            
             ViewModel = new VM();
         }
 
-        private async void BasePage_LoadedAsync(object sender, RoutedEventArgs e)
-        {
-            if (ShouldAnimateOut)
-                // Animate out the page
-                await AnimateOutAsync();
-            // Otherwise...
-            else
-                // Animate the page in
-                await AnimateInAsync();
-        }
 
-        private async Task AnimateInAsync()
-        {
-            if (PageLoadAnimation == PageAnimation.None)
-                return;
-
-            switch (PageLoadAnimation)
-            {
-                case PageAnimation.SlideAndFadeInFromRight:
-
-                    await this.SlideAndFadeInFromRightAsync(SlideSeconds);
-
-                    break;
-
-
-                case PageAnimation.SlideAndFadeOutToLeft:
-
-                    break;
-            }
-        }
-
-        private async Task AnimateOutAsync()
-        {
-            if (PageUnloadAnimation == PageAnimation.None)
-                return;
-
-            switch (PageUnloadAnimation)
-            {
-                case PageAnimation.SlideAndFadeInFromRight:
-
-
-                    break;
-
-
-                case PageAnimation.SlideAndFadeOutToLeft:
-
-                    await this.SlideAndFadeOutToLeftAsync(SlideSeconds);
-
-                    break;
-            }
-        }
 
     }
 }
